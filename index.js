8ad9a96fd17906a4e7f820517889bb29bdb236c8
Alessandro Tambellini <alessandrotambellini03@gmail.com>
2025-01-25 17:00:22 +0100

    build the garden layout

---
+++ index.js
@@ -0,0 +1,18 @@
+/* 
+take the body, 
+its width,
+how many squares can fit inside,
+add this quantity of squares into a row
+*/
+
+// const body = document.body;
+// let body_width = body.offsetWidth;
+// const cell_size = 40;
+// const cells_per_row = Math.floor(body_width/cell_size);
+
+// const row = document.querySelector('.row'); // i take the first one
+// for (let i = 0; i < cells_per_row; i++) {
+//     const cell = document.createElement('span');
+//     cell.classList.add('cell');
+//     row.appendChild(cell);
+// }
